e12ef7b574ab5d0b38c639fa9bc5f81c51b6545a
chen3283891376 <3283891376@qq.com>
2025-06-16 22:27:49 +0800

    🐛fix: 修正本地开发代理配置

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                // target: 'http://127.0.0.1:5000/',
-                target: 'https://chenify.pythonanywhere.com/',
+                target: 'http://127.0.0.1:5000/',
+                // target: 'https://chenify.pythonanywhere.com/',
                 changeOrigin: true,
             },
         },
